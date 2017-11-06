674dcd56830f11220ed0a7850f919b248364b121
audun hauge <audun@localhost.localdomain>
2017-11-06 22:41:07 +0100

    can now walk the map

--- civ.js
+++ civ.js
@@ -1,21 +1,105 @@
+
 function setup() {
     let divMain = document.getElementById("main");
-    let terrain = "grass,hill,swamp,forest,mountain,desert,snow".split(",");
+    let divBoard = document.getElementById("board");
+    let terrain = "grass,hill,swamp,forest,mountain,desert,sea,snow".split(",");
 
 
 
-    for (let i = 0; i < 9; i++) {
+    const W = 150;
+    const H = 90;
+    let brett = [];
+    let board = [];
+    let px = 30;
+    let py = 30;
+
+    for (let i = 0; i < W; i++) {
+        brett[i] = [];
+        for (let j = 0; j < H; j++) {
+            let type;
+            if (i === 0 || i + 1 === W || j === 0 || j + 1 === W) {
+                type = terrain.length -1;
+            } else {
+                type = Math.floor(Math.random() * (terrain.length - 1));
+            }
+            brett[i][j] = type;
+        }
+    }
+
+    for (let i = 0; i < 17; i++) {
+        board[i] = [];
         for (let j = 0; j < 9; j++) {
             let t = document.createElement('div');
-            t.className = "hex " + rndTerrain(terrain);
+            t.className = "hex";
             t.style.top = (-170 + j * 170) + "px";
-            t.style.left = (-198 + i * 198 + 100 * (j % 2)) + "px";
-            divMain.appendChild(t);
+            t.style.left = (-198 * 7 + i * 198 + 100 * j) + "px";
+            divBoard.appendChild(t);
+            board[i][j] = t;
         }
     }
 
 
-    
+
+    function render(px, py) {
+        for (let i = 0; i < 17; i++) {
+            for (let j = 0; j < 9; j++) {
+                let t = board[i][j];
+                let tile = brett[(px + i) % W][(py + j) % H];
+                t.className = "hex " + terrain[tile];
+            }
+        }
+
+    }
+
+    render(px, py);
+
+    document.addEventListener("keyup", move);
+
+    let wait = false;
+
+    function move(e) {
+        if (wait) return;
+        let dx = 0, dy = 0;
+        wait = true;
+        switch (e.keyCode) {
+            case 38:
+                py = (py + H - 1) % H;
+                dx = 100; dy = 170;
+                break;
+            case 40:
+                py = (py + 1) % H;
+                dx = -100; dy = -170;
+                break;
+            case 37:
+                px = (px + W - 1) % W;
+                dx = 200; dy = 0;
+                break;
+
+            case 39:
+                px = (px + 1) % W;
+                dx = -200; dy = 0;
+                break;
+            default:
+                return;
+        }
+        event.preventDefault();
+
+        let smooth = divBoard.animate(
+            [
+                { left: "0px", top: "0px" },
+                { left: dx + "px", top: dy + "px" }
+            ], {
+                duration: 500,
+            }
+        )
+
+        smooth.onfinish = () => {
+            render(px, py);
+            wait = false;
+        }
+
+    }
+
 }
 
 
